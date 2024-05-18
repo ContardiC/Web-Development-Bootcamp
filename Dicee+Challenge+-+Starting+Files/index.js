1047f7a0deff8420b144630f06b9173eaf9fd031
Carlo Contardi <carlo.contardi@isisbernocchi.edu.it>
2024-05-18 10:11:58 +0200

    index.js added

---
+++ Dicee+Challenge+-+Starting+Files/index.js
@@ -0,0 +1,11 @@
+alert("working");
+/*
+function randomGeneration(){
+    let randomNumber1 = Math.floor((Math.random()*6)+1);
+    let randomDice1 = "./images/dice"+randomNumber1+".png";
+    document.querySelector("img1").setAttribute("src",randomDice1);
+    let randomNumber2 = Math.floor((Math.random()*6)+1);
+    let randomDice2 = "./images/dice"+randomNumber1+".png";
+    document.querySelector("img3").setAttribute("src",randomDice2);
+}
+*/
