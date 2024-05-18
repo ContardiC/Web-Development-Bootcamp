5e59a712e9c66a212f51a0cead347201f7308539
Carlo Contardi <carlo.contardi@isisbernocchi.edu.it>
2024-05-18 10:32:35 +0200

    index.js updated

--- Dicee+Challenge+-+Starting+Files/index.js
+++ Dicee+Challenge+-+Starting+Files/index.js
@@ -1,8 +1,13 @@
 var randomNumber1 = Math.floor(Math.random() * 6) + 1; // 1-6
 var randomDiceImage = "dice" + randomNumber1 + ".png"; // dice1.png - dice6.png
-var randomImageSource = "/images/" + randomDiceImage; // images/dice1.png - images/dice6.png
+var randomImageSource = "images/" + randomDiceImage; // images/dice1.png - images/dice6.png
 
 var image1 = document.querySelectorAll("img")[0];
 image1.setAttribute("src",randomImageSource);
 
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+var randomDiceImage2 = "dice" + randomNumber2 + ".png";
+var randomImageSource2 = "images/" + randomDiceImage2;
 
+var image2 = document.querySelectorAll("img")[1];
+image2.setAttribute("src",randomImageSource2);
