f4f50852b8f41ddd946f4e843e7992f72d08a0d9
Carlo Contardi <carlo.contardi@isisbernocchi.edu.it>
2024-05-19 16:18:44 +0200

    jQuery examples

--- jQuery/index.js
+++ jQuery/index.js
@@ -1,5 +1,11 @@
 // con document ready controlliamo se la libreria jQuery è stata letta ed è pronta per essere usata
 // oppure includere gli script prima della chiusura del tag body
+/*
 $(document).ready(function(){
-    $("h1").css("color","red");
+    
 });
+*/
+console.log($("h1").css("color"));
+console.log($("h1").css("font-size"));
+$("h1").css("color","red");
+$("h1").addClass("big-title");
