23934b357ef8a985e113fb192e41d9d0a621b70e
Carlo Contardi <carlo.contardi@isisbernocchi.edu.it>
2024-05-19 16:30:52 +0200

    jQuery per modificare lo stile inserito

--- jQuery/index.js
+++ jQuery/index.js
@@ -5,7 +5,16 @@ $(document).ready(function(){
     
 });
 */
+// MODIFICARE LO STILE
 console.log($("h1").css("color"));
 console.log($("h1").css("font-size"));
-$("h1").css("color","red");
+//$("h1").css("color","red");
+// Meglio separare lo stile e usare le classi nel seguente modo
 $("h1").addClass("big-title");
+$("h1").removeClass("big-tile");
+//per aggiungere più classi separarle con lo spazio
+$("h1").addClass("bit-title margin-50 ");
+// per sapere se ha una classe
+console.log($("h1").hasClass("margin-50"));
+
+// MODIFICARE IL TESTO 
