975ea8e0db43d9b3e04d6b89eeb16e0b04fb1a64
Carlo Contardi <carlo.contardi@isisbernocchi.edu.it>
2024-05-19 10:59:35 +0200

    jQuery folder added

--- jQuery/index.js
+++ jQuery/index.js
@@ -1,1 +1,5 @@
-// alert("Working!");
+// con document ready controlliamo se la libreria jQuery è stata letta ed è pronta per essere usata
+// oppure includere gli script prima della chiusura del tag body
+$(document).ready(function(){
+    $("h1").css("color","red");
+});
