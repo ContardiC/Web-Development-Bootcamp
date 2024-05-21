86b0c507a80e4d587016ad694e0c0e7c61ec79f2
Carlo Contardi <carlo.contardi@isisbernocchi.edu.it>
2024-05-21 17:08:51 +0200

    nextSequence() function added

---
+++ SimonGameChallengeStartingFiles/game.js
@@ -0,0 +1,10 @@
+//alert("The game is born!!!!");
+
+var buttonColors = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+
+function nextSequence() {
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColors[randomNumber];
+  gamePattern.push(randomChosenColour);
+}
