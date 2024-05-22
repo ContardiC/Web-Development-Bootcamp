29c259e3bd7c60653f899099a552769bcf0719e8
Carlo Contardi <carlo.contardi@isisbernocchi.edu.it>
2024-05-22 09:15:45 +0200

    sounds and animation added

--- SimonGameChallengeStartingFiles/game.js
+++ SimonGameChallengeStartingFiles/game.js
@@ -2,9 +2,33 @@
 
 var buttonColors = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
-
+var userClickedPattern = [];
 function nextSequence() {
   var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColour = buttonColors[randomNumber];
   gamePattern.push(randomChosenColour);
+  $("#" + randomChosenColour)
+    .fadeIn(100)
+    .fadeOut(100)
+    .fadeIn(100);
+  playSound(randomChosenColour);
 }
+
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+function animatePress(currentColour){
+  $("#"+currentColour).addClass("pressed");
+  setTimeout(()=>{
+    $("#"+currentColour).removeClass("pressed");
+  },100);
+}
+
+$(".btn").click(function () {
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+  console.log(userClickedPattern);
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+});
