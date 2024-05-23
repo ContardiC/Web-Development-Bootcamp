e782524073e118cf8bb8e49a5d4c18d903a6a907
Carlo Contardi <carlo.contardi@isisbernocchi.edu.it>
2024-05-23 17:21:21 +0200

    npm exercise added

---
+++ Backend/2.3 NPM/index.js
@@ -0,0 +1,5 @@
+var genereteName = require("sillyname");
+var sillyName = genereteName();
+// uso l'interpolazione delle stringhe di js usando ${variabile_stringa} 
+// IMPORTANTE usare ` ` per racchiudere le stringhe 
+console.log(`My name is ${sillyName}.`);
