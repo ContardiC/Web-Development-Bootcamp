a585bbd34982705cdf40126f9464ee2ca8fb7798
ContardiC <c.contardi@spacecoding.it>
2024-05-24 11:20:57 +0200

    QR Project added

--- Backend/2.3 NPM/index.js
+++ Backend/2.3 NPM/index.js
@@ -1,9 +1,12 @@
-
 // CommonJS import
 // var genereteName = require("sillyname");
 // ECMAScript 6 import
 import generateName from "sillyname";
+import superheroes from "superheroes";
+
 var sillyName = generateName();
-// uso l'interpolazione delle stringhe di js usando ${variabile_stringa} 
-// IMPORTANTE usare ` ` per racchiudere le stringhe 
+var superheroName = superheroes.random();
+// uso l'interpolazione delle stringhe di js usando ${variabile_stringa}
+// IMPORTANTE usare ` ` per racchiudere le stringhe
 console.log(`My name is ${sillyName}.`);
+console.log(`My name is ${superheroName}!`);
