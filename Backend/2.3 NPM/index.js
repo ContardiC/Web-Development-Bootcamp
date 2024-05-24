62af608c5fef51c79fdc7f8f911c3eb3012f8ca1
ContardiC <c.contardi@spacecoding.it>
2024-05-24 10:18:51 +0200

    Change from CommonJS to ECMAScript import

--- Backend/2.3 NPM/index.js
+++ Backend/2.3 NPM/index.js
@@ -1,5 +1,9 @@
-var genereteName = require("sillyname");
-var sillyName = genereteName();
+
+// CommonJS import
+// var genereteName = require("sillyname");
+// ECMAScript 6 import
+import generateName from "sillyname";
+var sillyName = generateName();
 // uso l'interpolazione delle stringhe di js usando ${variabile_stringa} 
 // IMPORTANTE usare ` ` per racchiudere le stringhe 
 console.log(`My name is ${sillyName}.`);
