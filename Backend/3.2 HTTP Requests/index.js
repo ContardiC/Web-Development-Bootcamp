bfa32ef5bdba588f7ecc8c623537f5501c874e69
Carlo Contardi <carlo.contardi@isisbernocchi.edu.it>
2024-05-25 19:00:48 +0200

    HTTP Requests init

---
+++ Backend/3.2 HTTP Requests/index.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.listen(port,()=>{
+    console.log(`Server running on port ${port}.`);
+})
+
+app.get("/",(req,res)=>{
+    res.send("hello");
+});
