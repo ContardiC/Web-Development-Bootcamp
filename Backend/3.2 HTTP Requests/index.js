6d9fc2af42c9f85f2ef5eefd54c92682088d2a52
Carlo Contardi <carlo.contardi@isisbernocchi.edu.it>
2024-05-25 19:11:25 +0200

    managed different /Endopoint in the backend 3.2 HTTP Requests

--- Backend/3.2 HTTP Requests/index.js
+++ Backend/3.2 HTTP Requests/index.js
@@ -5,8 +5,16 @@ const port = 3000;
 
 app.listen(port,()=>{
     console.log(`Server running on port ${port}.`);
-})
+});
 
 app.get("/",(req,res)=>{
-    res.send("hello");
+    res.send("<h1>Hello World</h1>");
+});
+
+app.get("/contact",(req,res)=>{
+    res.send("<h1>Contact Page</>");
+});
+
+app.get("/about",(req,res)=>{
+    res.send("<h1>About Page</>");
 });
