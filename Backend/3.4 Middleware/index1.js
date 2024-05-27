0b3162f041d008ea0b7d029dcaf0ae7f0faec6ca
Carlo Contardi <carlo.contardi@isisbernocchi.edu.it>
2024-05-27 16:38:30 +0200

    Middleware exercise added

--- Backend/3.4 Middleware/index1.js
+++ Backend/3.4 Middleware/index1.js
@@ -3,14 +3,19 @@ import express from "express";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 const __dirname = dirname(fileURLToPath(import.meta.url));
-
+import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
+app.use(bodyParser.urlencoded({extended:true}));
+// /submit e' l'endopoint a cui punta l'action del form   <form action="/submit" method="POST">
+app.post("/submit",(req,res)=>{
+  console.log(req.body);
+});
 
 app.get("/", (req, res) => {
   //nel caso il sito sia ospitato su un cloud non basta il percorso locale del file
   // ma dobbiamo importare dirname e fileURLToPath
-  console.log(__dirname + "/public/index.html")
+  // console.log(__dirname + "/public/index.html")
   res.sendFile(__dirname + "/public/index.html");
 });
 
