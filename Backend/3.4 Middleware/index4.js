46d300cd94bf5e2a3798fa9edd444a872d47aa25
Carlo Contardi <carlo.contardi@isisbernocchi.edu.it>
2024-05-28 18:18:30 +0200

    updated

--- Backend/3.4 Middleware/index4.js
+++ Backend/3.4 Middleware/index4.js
@@ -7,9 +7,10 @@ const app = express();
 const port = 3000;
 
 var bandName = "";
+// Middleware bodyparser per primo !!! se si cambia l'ordine si generano errori
 app.use(bodyParser.urlencoded({extended:true}));
 
-
+// preprocessing Middleware
 function bandNameGenerator(req,res,next){
   console.log(req.body);
   bandName = req.body["street"] + req.body["pet"];
