f5961dd1e3b40f9351279cdde28ac84209666ab5
Carlo Contardi <carlo.contardi@isisbernocchi.edu.it>
2024-05-29 16:46:50 +0200

    EJS exercise 1 done

---
+++ Backend/4.0 EJS/index.js
@@ -0,0 +1,24 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  const today = new Date();
+  const day = today.getDay();
+  // console.log(day);
+  let type = "a weekday";
+  let adv = "It's time to work hard";
+  if (day === 0 || day === 6) {
+    type = "the weekend";
+    adv = "It's time to have some fun";
+  }
+  res.render("index.ejs", {
+    dateType: type,
+    advice: adv,
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
