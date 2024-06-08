d9e0476eea68295e2737ec86f363142872881c26
ContardiC <c.contardi@spacecoding.it>
2024-06-08 16:14:36 +0200

    end Axios exercise

--- Backend/5.3-Axios/index.js
+++ Backend/5.3-Axios/index.js
@@ -26,10 +26,27 @@ app.get("/", async (req, res) => {
 
 app.post("/", async (req, res) => {
   console.log(req.body);
+  const type = req.body.type;
+  const participants = req.body.participants;
+  
+    try{
+      const response = await axios.get(`https://bored-api.appbrewery.com/filter?type=${type}&participants=${participants}`);
+      const result = response.data;
+     
+      res.render("index.ejs", { data: result[Math.floor(Math.random()*result.length)],
 
+       });
+    }catch(error){
+      console.error("Failed to make request:", error.message);
+    res.render("index.ejs", {
+      error: "No activities that match your criteria.",
+    });
+    } 
+  
   // Step 2: Play around with the drop downs and see what gets logged.
   // Use axios to make an API request to the /filter endpoint. Making
   // sure you're passing both the type and participants queries.
+ 
   // Render the index.ejs file with a single *random* activity that comes back
   // from the API request.
   // Step 3: If you get a 404 error (resource not found) from the API request.
