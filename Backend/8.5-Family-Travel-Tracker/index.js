0faf9a18ac4d6575c086c434f70d480bace29eef
ContardiC <c.contardi@spacecoding.it>
2024-06-17 17:23:15 +0200

    add Family Travel Tracker Project

--- Backend/8.5-Family-Travel-Tracker/index.js
+++ Backend/8.5-Family-Travel-Tracker/index.js
@@ -17,6 +17,13 @@ db.connect();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+let currentUserId = 1;
+
+let users = [
+  { id: 1, name: "Angela", color: "teal" },
+  { id: 2, name: "Jack", color: "powderblue" },
+];
+
 async function checkVisisted() {
   const result = await db.query("SELECT country_code FROM visited_countries");
   let countries = [];
@@ -25,13 +32,15 @@ async function checkVisisted() {
   });
   return countries;
 }
-// GET home page
 app.get("/", async (req, res) => {
   const countries = await checkVisisted();
-  res.render("index.ejs", { countries: countries, total: countries.length });
+  res.render("index.ejs", {
+    countries: countries,
+    total: countries.length,
+    users: users,
+    color: "teal",
+  });
 });
-
-//INSERT new country
 app.post("/add", async (req, res) => {
   const input = req.body["country"];
 
@@ -51,22 +60,16 @@ app.post("/add", async (req, res) => {
       res.redirect("/");
     } catch (err) {
       console.log(err);
-      const countries = await checkVisisted();
-      res.render("index.ejs", {
-        countries: countries,
-        total: countries.length,
-        error: "Country has already been added, try again.",
-      });
     }
   } catch (err) {
     console.log(err);
-    const countries = await checkVisisted();
-    res.render("index.ejs", {
-      countries: countries,
-      total: countries.length,
-      error: "Country name does not exist, try again.",
-    });
   }
+});
+app.post("/user", async (req, res) => {});
+
+app.post("/new", async (req, res) => {
+  //Hint: The RETURNING keyword can return the data that was inserted.
+  //https://www.postgresql.org/docs/current/dml-returning.html
 });
 
 app.listen(port, () => {
