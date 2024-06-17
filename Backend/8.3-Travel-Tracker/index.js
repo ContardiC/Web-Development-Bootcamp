909fe1551fa5cd57ad8fdeecb3d389666606a60e
ContardiC <c.contardi@spacecoding.it>
2024-06-17 16:49:15 +0200

    add /add route

--- Backend/8.3-Travel-Tracker/index.js
+++ Backend/8.3-Travel-Tracker/index.js
@@ -10,24 +10,47 @@ const db = new pg.Client({
   host: "localhost",
   database: "world",
   password: "root",
-  port: 5433,
+  port: 5432,
 });
 db.connect();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-// GET home page
-app.get("/", async (req, res) => {
-  //Write your code here.
+
+async function checkVisisted(){
   const result = await db.query("SELECT country_code FROM visited_countries");
   let countries = [];
   result.rows.forEach((country) => {
     countries.push(country.country_code);
   });
-  console.log(result.rows);
+  return countries;
+}
+
+// GET home page
+app.get("/", async (req, res) => {
+  //Write your code here.
+  const countries = await checkVisisted();
   res.render("index.ejs", { countries: countries, total: countries.length });
-  db.end();
+});
+// POST /add
+app.post("/add", async (req, res) => {
+  const input = req.body["country"];
+
+  const result = await db.query(
+    "SELECT country_code FROM countries WHERE country_name = $1",
+    [input]
+  );
+
+  if (result.rows.length !== 0) {
+    const data = result.rows[0];
+    const countryCode = data.country_code;
+
+    await db.query("INSERT INTO visited_countries (country_code) VALUES ($1)", [
+      countryCode,
+    ]);
+    res.redirect("/");
+  }
 });
 
 app.listen(port, () => {
