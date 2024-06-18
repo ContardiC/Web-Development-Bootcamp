a066923bd0fc708fb928d9242ea6c5d4a7f35f01
ContardiC <c.contardi@spacecoding.it>
2024-06-18 17:23:04 +0200

    update FTT Project

--- Backend/8.5-Family-Travel-Tracker/index.js
+++ Backend/8.5-Family-Travel-Tracker/index.js
@@ -9,7 +9,7 @@ const db = new pg.Client({
   user: "postgres",
   host: "localhost",
   database: "world",
-  password: "123456",
+  password: "root",
   port: 5432,
 });
 db.connect();
@@ -19,26 +19,31 @@ app.use(express.static("public"));
 
 let currentUserId = 1;
 
-let users = [
-  { id: 1, name: "Angela", color: "teal" },
-  { id: 2, name: "Jack", color: "powderblue" },
-];
+ let users = [];
 
 async function checkVisisted() {
-  const result = await db.query("SELECT country_code FROM visited_countries");
+  const result = await db.query("SELECT country_code FROM visited_countries JOIN users ON users.id = user_id WHERE user_id = $1",[currentUserId]);
   let countries = [];
   result.rows.forEach((country) => {
     countries.push(country.country_code);
   });
   return countries;
 }
+
+async function getCurrentUser(){
+  const result = await db.query("SELECT * FROM users");
+  users = result.rows;
+  // se i dati vengono forniti da un form spesso anche l'id potrebbe essere una stringa quindi utilizzare == e non ===
+  return users.find((user) => user.id == currentUserId);
+}
 app.get("/", async (req, res) => {
   const countries = await checkVisisted();
+  const currentUser = await getCurrentUser();
   res.render("index.ejs", {
     countries: countries,
     total: countries.length,
     users: users,
-    color: "teal",
+    color: currentUser.color,
   });
 });
 app.post("/add", async (req, res) => {
@@ -54,8 +59,8 @@ app.post("/add", async (req, res) => {
     const countryCode = data.country_code;
     try {
       await db.query(
-        "INSERT INTO visited_countries (country_code) VALUES ($1)",
-        [countryCode]
+        "INSERT INTO visited_countries (country_code, user_id) VALUES ($1,$2)",
+        [countryCode, currentUserId]
       );
       res.redirect("/");
     } catch (err) {
@@ -65,7 +70,23 @@ app.post("/add", async (req, res) => {
     console.log(err);
   }
 });
-app.post("/user", async (req, res) => {});
+app.post("/user", async (req, res) => {
+  // id dello user selezionato tramite il menu a tabs
+  //console.log(req.body["user"]);  
+  currentUserId = req.body["user"];
+  
+  const countries = await checkVisisted();
+  const users = await loadUsers();
+  console.log(JSON.stringify(users[currentUserId]));
+  
+  //const color = users[currentUserId]["color"];
+  res.render("index.ejs", {
+    countries: countries,
+    total: countries.length,
+    users: users,
+    color: "red",
+  });
+});
 
 app.post("/new", async (req, res) => {
   //Hint: The RETURNING keyword can return the data that was inserted.
