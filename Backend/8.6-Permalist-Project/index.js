1b2c15da3c9e0c46a51bb30850ed0966741ecf6a
Carlo Contardi <c.contardi@spacecoding.it>
2024-06-20 09:45:02 +0200

    add db connection

--- Backend/8.6-Permalist-Project/index.js
+++ Backend/8.6-Permalist-Project/index.js
@@ -1,8 +1,19 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
 
 const app = express();
 const port = 3000;
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "permalist",
+  password: "root",
+  port: 5432,
+});
+
+db.connect();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
